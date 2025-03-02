a5eb3464a9fb6ad35148373b07b4b58c330f36c8
RattusDong <147535283+RattusDong@users.noreply.github.com>
2025-03-02 14:44:12 +0000

    Update player anim
    player now faces last moved direction when idle

--- Assets/Player/PlayerAnimationHandler.cs
+++ Assets/Player/PlayerAnimationHandler.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AnimationHandler : MonoBehaviour
@@ -18,11 +19,20 @@ public class AnimationHandler : MonoBehaviour
     void Update()
     {
         Vector3 velocity = transform.position - lastPos;
-        
+
         // using 2d symantics 
 
+        //for walk anims
         animator.SetFloat("Xv", velocity.x * 100);
         animator.SetFloat("Yv", velocity.z * 99);
+
+        //for idle anims
+        if (MathF.Abs(velocity.x) > 0 || MathF.Abs(velocity.z) > 0)
+        {
+            animator.SetFloat("Xi", velocity.x * 100);
+            animator.SetFloat("Yi", velocity.z * 99);
+        }
+
         lastPos = transform.position;
     }
 }
